c478116837fe07abd6f83d725af9297ee8967fa1
Sdsurve <sdsurve55555@gmail.com>
2024-06-24 01:20:54 +0530

    add to cards css and jsx

--- src/components/ToDoCards/ToDoCards.jsx
+++ src/components/ToDoCards/ToDoCards.jsx
@@ -23,7 +23,24 @@ function ToDoCards({ index, task, category, deleteItem }) {
   };
 
   return (
-
+    <div className='todo-card'>
+      <img 
+        src={ImgDel} 
+        alt="Delete" 
+        className='delete-icon' 
+        onClick={() => {
+          deleteItem(index);
+        }}
+      />
+      {task}
+      <span 
+        className='category' 
+        style={{
+          backgroundColor: CATEGORY_COLOURS[category] || 'grey'
+        }}>
+        {CATEGORY_EMOJI_MAP[category] || ''} {category}
+      </span>
+    </div>
   );
 }
 
